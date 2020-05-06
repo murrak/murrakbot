e1c19a0c405546520ebcc3be4f1192757d3e62c6
murrak <64873828+murrak@users.noreply.github.com>
2020-05-06 22:26:09 +0900

    Update py.py

--- py.py
+++ py.py
@@ -27,5 +27,5 @@ async def on_message(message):
        await message.channel.send("https://www.twitch.tv/murrak")
 
 
-access_token = os.environ[BOT_TOKEN]
-client.run("access_token")
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
